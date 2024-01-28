efbc1121f598c19d08dbb40820fd1275b8603675
lamabeta <148013077+lamabeta@users.noreply.github.com>
2024-01-28 15:54:53 +0300

    Update DoublyLinkedList.java

--- src/DoublyLinkedList.java
+++ src/DoublyLinkedList.java
@@ -1,19 +1,10 @@
-class Node {
-    int data;
-    Node prev;
-    Node next;
-
-    Node(int data) {
-        this.data = data;
-    }
-}
-
 public class DoublyLinkedList{
-   public static void linearSearch(Node head, int target) {
+   Node head;
+   public static void linearSearch(int target) {
           /*add your code here*/
    }
    
    public static void main(String[] args) {
-        /* call  the function */  
+        linearSearch(9)
    }
 }
